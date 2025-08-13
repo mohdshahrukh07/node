44e394e846dabb45c7cebc60de5ab62cde5ac933
complibuedsk <complibuedsk@gmail.com>
2025-08-13 11:04:48 +0530

    add getWatchHistory functionality and update user routes to include it

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,18 @@
-import {Router} from "express";
-import {loginUser, logoutUser, registerUser,refreshAccessToken} from "../controllers/user.controller.js"
-import {upload} from "../middlewares/multer.middleware.js"
+import { Router } from "express";
+import { 
+    loginUser,
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { varifyJWT } from "../middlewares/auth.middleware.js";
 
 
@@ -8,18 +20,25 @@ const router = Router()
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name:"coverImage",
-            maxCount:1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
     registerUser)
 router.route("/login").post(loginUser)
 
 //secured routes
-router.route("/logout").post(varifyJWT,logoutUser)
+router.route("/logout").post(varifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(varifyJWT, changeCurrentPassword);
+router.route("/current-user").get(varifyJWT, getCurrentUser);
+router.route("/update-account").patch(varifyJWT, updateAccountDetails);
+router.route("/avatar").patch(varifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(varifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(varifyJWT, getUserChannelProfile);
+router.route("/history").get(varifyJWT, getWatchHistory);
 export default router;
